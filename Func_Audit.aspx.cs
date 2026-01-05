268f20db7693ac459a3b943b8d2ca2845cb42a6a
alextanyc-veecotech <yenchean@veecotech.com>
2026-01-05 19:13:03 +0800

    TRA-F27847 Cross-Site Scripting (Reflective)

--- Func_Audit.aspx.cs
+++ Func_Audit.aspx.cs
@@ -8,13 +8,24 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using System.Text.RegularExpressions;
 
 public partial class Func_Audit : System.Web.UI.Page
 {
     public string token;
     protected void Page_Load(object sender, EventArgs e)
     {
-        Session["fid"] = Request["fid"];
+        string fid = Request["fid"];
+        string link = Request["link"];
+        string parentid = Request["parentid"];
+
+        if (!IsValidFid(fid) || !IsValidParentId(parentid) || !IsValidLink(link))
+        {
+            Response.Redirect("~/Admin_Error.aspx");
+            return;
+        }
+
+        Session["fid"] = fid;
 		
         if (Session["USER_ID"] != null)
         {
@@ -22,10 +33,8 @@ public partial class Func_Audit : System.Web.UI.Page
             token = Guid.NewGuid().ToString() + "-" + Session.SessionID;
             Session["token"] = token;
 			/*Response.Write (token); */
-            string fid = Convert.ToString(Request["fid"]);
-            string link = Convert.ToString(Request["link"]);
-            Session["fidlink"] = "Func_Audit.aspx?link=" + Request["Link"].ToString() + "&fid=" + Request["fid"].ToString();
-			Page.ClientScript.RegisterStartupScript(this.GetType(), "fidscript", "postForEditMenu1('" + Request["link"].ToString() + "','" + Request["fid"].ToString() + "','" + token + "');", true);
+            Session["fidlink"] = "Func_Audit.aspx?link=" + HttpUtility.UrlEncode(Request["Link"].ToString()) + "&fid=" + HttpUtility.UrlEncode(fid);
+			Page.ClientScript.RegisterStartupScript(this.GetType(), "fidscript", "postForEditMenu1('" + EscapeJsString(link) + "','" + EscapeJsString(fid) + "','" + EscapeJsString(token) + "');", true);
         }
         else
         {
@@ -34,5 +43,31 @@ public partial class Func_Audit : System.Web.UI.Page
         }
 
 
+    }
+
+    private bool IsValidFid(string fid)
+    {
+        if (string.IsNullOrEmpty(fid)) return false;
+        return Regex.IsMatch(fid, @"^[a-zA-Z0-9]+$");
+    }
+
+    private bool IsValidParentId(string parentid)
+    {
+        if (string.IsNullOrEmpty(parentid)) return true; // assuming optional
+        int val;
+        return int.TryParse(parentid, out val);
+    }
+
+    private bool IsValidLink(string link)
+    {
+        if (string.IsNullOrEmpty(link)) return false;
+        // Allow alphanumeric, forward slashes, dots, hyphens
+        return Regex.IsMatch(link, @"^[a-zA-Z0-9/\.\-]+$");
+    }
+
+    private string EscapeJsString(string s)
+    {
+        if (s == null) return "";
+        return s.Replace("\\", "\\\\").Replace("'", "\\'").Replace("\"", "\\\"").Replace("\r", "\\r").Replace("\n", "\\n");
     }
 }
